5e405b55d83b180be33ae675c6084556b376c2f0
Erick Garcia <garciaerickbonilla@gmail.com>
2022-06-07 23:26:26 -0600

    cambion

--- Ejercicios/Ejercicio2 - Biblioteca/biblioteca.cpp
+++ Ejercicios/Ejercicio2 - Biblioteca/biblioteca.cpp
@@ -6,10 +6,10 @@ string libros[2][2];
 
 void cargarlibros() {
     libros[0][0] = "Algoritmos";
-    libros[0][1] = "Algoritmos y Progrmacion (Guia para Docentes)";
+    libros[0][1] = "Algoritmos y Programacion(Guia para Docentes)";
 
     libros[1][0] = "Base de datos";
-    libros[1][1] = "Apuntes de base de datps";
+    libros[1][1] = "Apuntes de base de datos";
 
 }
 
@@ -17,10 +17,46 @@ int main(int argc, char const *argv[])
 {
     cargarlibros ();
 
-    for (int  i = 0; i < 2; i++)
-    {
-        cout << libros[i][1];
-    }
+   bool salir = false;
+
+   while (salir = false)
+   {
+     string buscar = "";
+     system("cls");
+     cout << "Ingrese la descripcion del libro que busca: ";
+     cin>> buscar;
+
+     
+     for (int i = 0; i < 2; i++)
+     {
+        string libro = libros [i][1];
+
+        if (libro.find(buscar) != string::npos) {
+            cout << "Libro encontrado:" << libro << endl;
+            salir = true;
+        }
+        
+     }
+     
+
+     if (salir == false) {
+         char continuar = 'n';
+         while (true) {
+            system("cls");
+            cout << "No se encontro el libro que busca. Desea continuar? s/n";
+            cin >> continuar;
+
+            if (continuar == 's' || continuar == 'S') {
+                break;
+            } else if  (continuar == 'n' || continuar == 'N'){
+                break;
+            }
+         }
+         
+     }
+
+   }
+   
     
     return 0;
 }
