26c22aadf30061ae4efc1c969a2ece5ceb4b361c
Erick Garcia <garciaerickbonilla@gmail.com>
2022-06-07 22:04:45 -0600

    cambio

--- Ejercicios/Ejercicio2 - Biblioteca/biblioteca.cpp
+++ Ejercicios/Ejercicio2 - Biblioteca/biblioteca.cpp
@@ -2,9 +2,25 @@
 
 using namespace std;
 
+string libros[2][2];
+
+void cargarlibros() {
+    libros[0][0] = "Algoritmos";
+    libros[0][1] = "Algoritmos y Progrmacion (Guia para Docentes)";
+
+    libros[1][0] = "Base de datos";
+    libros[1][1] = "Apuntes de base de datps";
+
+}
+
 int main(int argc, char const *argv[])
 {
-    cout << "Hola";
+    cargarlibros ();
+
+    for (int  i = 0; i < 2; i++)
+    {
+        cout << libros[i][1];
+    }
     
     return 0;
 }
